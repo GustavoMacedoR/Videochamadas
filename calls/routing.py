ec08e0f0841b84feabbfb4cba2af4960339e48ec
GustavoMacedoR <gustavogpi25@outlook.com>
2026-01-26 21:51:20 -0300

    configurações do nginx e do docker compose

--- calls/routing.py
+++ calls/routing.py
@@ -2,5 +2,6 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/call/(?P<room_name>[^/]+)/$', consumers.CallConsumer.as_asgi()),
+    # use /ws/calls/ to avoid colliding with an existing /ws/ proxy
+    re_path(r'ws/calls/(?P<room_name>[^/]+)/$', consumers.CallConsumer.as_asgi()),
 ]
