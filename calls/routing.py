c7394bf48a93fd58d1a6600c6f3a5fe9fd8562fa
GustavoMacedoR <gustavogpi25@outlook.com>
2026-03-02 21:07:35 -0300

    qqqqqqqqqqqqqqq

--- calls/routing.py
+++
@@ -1,7 +0,0 @@
-from django.urls import re_path
-from . import consumers
-
-websocket_urlpatterns = [
-    # use /ws/calls/ to avoid colliding with an existing /ws/ proxy
-    re_path(r'ws/calls/(?P<room_name>[^/]+)/$', consumers.CallConsumer.as_asgi()),
-]
